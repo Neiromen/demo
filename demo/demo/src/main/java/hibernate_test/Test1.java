476f8ef76df37c1d7083c5a5d044a9cf1df5da00
Neiromen <alex.kor0702@gmail.com>
2025-06-12 22:41:35 +0300

    My first hibernate success usage

--- demo/demo/src/main/java/hibernate_test/Test1.java
+++ demo/demo/src/main/java/hibernate_test/Test1.java
@@ -1,6 +1,7 @@
 package hibernate_test;
 
 import hibernate_test.entity.Employee;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
@@ -8,8 +9,19 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class Test1 {
     public static void main(String[] args) {
-        SessionFactory configuration = new Configuration()
+        SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(Employee.class).buildSessionFactory();
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+            Session session = factory.getCurrentSession();
+            Employee employee = new Employee("Alexandr","Korobko","IT",500);
+            session.beginTransaction();
+            session.save(employee);
+            session.getTransaction().commit();
+        }
+        finally {
+            factory.close();
+        }
     }
 }
