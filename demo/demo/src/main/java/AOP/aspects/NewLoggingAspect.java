f4c9f41c4c3be51caa0b1d7916ff24bf50bbe56c
Neiromen <alex.kor0702@gmail.com>
2025-06-12 19:58:00 +0300

    Around advices

---
+++ demo/demo/src/main/java/AOP/aspects/NewLoggingAspect.java
@@ -0,0 +1,27 @@
+package AOP.aspects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class NewLoggingAspect {
+    @Around("execution(public String returnBook())")
+    public Object aroundReturnBookLoggingAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("aroundReturnBookLoggingAdvice: в библиотеку пытаются вернуть книгу");
+        Object targetMethodResult = null;
+        long begin = System.currentTimeMillis();
+        try{
+            targetMethodResult = proceedingJoinPoint.proceed();
+        }catch (Exception e){
+            System.out.println("aroundReturnBookLoggingAdvice: Поймано исключение: "+ e);
+            throw e;
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("aroundReturnBookLoggingAdvice: в библиотеку успешно вернули книгу");
+        System.out.println("aroundReturnBookLoggingAdvice: метод returnBook() выполнил работу за: "+(end-begin)+" миллисекунд");
+        return targetMethodResult;
+    }
+}
