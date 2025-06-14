82825cbe5e1ae78a97414e5d383d36d47f23cbe4
Neiromen <alex.kor0702@gmail.com>
2025-06-14 17:04:41 +0300

    One-to-One(Uni-directional)

---
+++ demo/demo/src/main/java/hibernate_test2/Test1.java
@@ -0,0 +1,52 @@
+package hibernate_test2;
+
+import hibernate_test2.Entity.Detail;
+import hibernate_test2.Entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class).addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+        try {
+//            Session session = factory.getCurrentSession();
+//            Employee employee = new Employee("Alexandr","Korobko","IT",700);
+//            Detail detail = new Detail("Baku","1234567890","lox.chilisky@gmail.com");
+//            employee.setEmpDetail(detail);
+//            session.beginTransaction();
+//
+//            session.save(employee);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+
+//            Session session = factory.getCurrentSession();
+//            Employee employee = new Employee("Oleg","Smirnov","Sales",700);
+//            Detail detail = new Detail("Moscow","9876543210","lox.indisky@gmail.com");
+//            employee.setEmpDetail(detail);
+//            session.beginTransaction();
+//
+//            session.save(employee);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+
+
+            session.beginTransaction();
+
+            Employee emp = session.get(Employee.class,2);
+            session.delete(emp);
+            session.getTransaction().commit();
+            System.out.println("Done!");
+
+        }
+        finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
